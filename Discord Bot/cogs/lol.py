559f557896b7aff59bb7f9ae97806cdad9c2f41b
Chue Zhang <32043313+Chuezhang2278@users.noreply.github.com>
2020-08-18 04:51:40 -0400

    update to $find
    very nice

--- Discord Bot/cogs/lol.py
+++ Discord Bot/cogs/lol.py
@@ -1,8 +1,20 @@
 import discord
+import json
 from discord.ext import commands
 from riotwatcher import LolWatcher, ApiError
 
-api_key = 'key'
+api_key = 'x'
+watcher = LolWatcher(api_key)
+my_region = 'na1'
+latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion']
+
+def dictName(participants, arr):
+
+    for i in participants:
+        arr[str(i['Summoner Name'])] = i
+
+    return arr 
+
 
 class League(commands.Cog):
     def __init__(self, client):
@@ -10,11 +22,8 @@ class League(commands.Cog):
     
     @commands.command()
     async def find(self, ctx, arg):
-        watcher = LolWatcher(api_key)
-        my_region = 'na1'
 
         me = watcher.summoner.by_name(my_region, arg)
-        latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion']
         static_champ_list = watcher.data_dragon.champions(latest, False, 'en_US')
         static_item_list = watcher.data_dragon.items(latest, 'en_US')
         my_matches = watcher.match.matchlist_by_account(my_region, me['accountId'])
@@ -23,6 +32,10 @@ class League(commands.Cog):
         participants, temp = [], []
 
         i = 0
+
+        f = open('itemlist.json',)
+        data = json.load(f)
+
         for name in match_detail['participantIdentities']:
             temp.append(name['player']['summonerName'])
 
@@ -37,29 +50,29 @@ class League(commands.Cog):
             i5 = row['stats']['item5']
 
             try:
-                participants_row['item0'] = static_item_list['data'][str(i0)]['name']
+                participants_row['item0'] = data[static_item_list['data'][str(i0)]['name']]
             except:
-                participants_row['item0'] = None
+                participants_row['item0'] = ""
             try:
-                participants_row['item1'] = static_item_list['data'][str(i1)]['name']
+                participants_row['item1'] = data[static_item_list['data'][str(i1)]['name']]
             except:
-                participants_row['item1'] = None
+                participants_row['item1'] = ""
             try:
-                participants_row['item2'] = static_item_list['data'][str(i2)]['name']
+                participants_row['item2'] = data[static_item_list['data'][str(i2)]['name']]
             except:
-                participants_row['item2'] = None
+                participants_row['item2'] = ""
             try:
-                participants_row['item3'] = static_item_list['data'][str(i3)]['name']
+                participants_row['item3'] = data[static_item_list['data'][str(i3)]['name']]
             except:
-                participants_row['item3'] = None
+                participants_row['item3'] = ""
             try:
-                participants_row['item4'] = static_item_list['data'][str(i4)]['name']
+                participants_row['item4'] = data[static_item_list['data'][str(i4)]['name']]
             except:
-                participants_row['item4'] = None
+                participants_row['item4'] = ""
             try:
-                participants_row['item5'] = static_item_list['data'][str(i5)]['name']
+                participants_row['item5'] = data[static_item_list['data'][str(i5)]['name']]
             except:
-                participants_row['item5'] = None
+                participants_row['item5'] = ""
 
             g = match_detail['gameDuration']
             g = (g//60) + ((g % 60)/100)
@@ -69,16 +82,14 @@ class League(commands.Cog):
             participants.append(participants_row)
 
             i += 1
-
-        arr = {}
+        
+        temp = {}
+        arr = dictName(participants,temp)
 
         embed = discord.Embed(colour = discord.Color.blue())
 
-        for i in participants:
-            arr[str(i['Summoner Name'])] = i 
-
         for i in temp:
-            embed.add_field(name=i, value = str(arr[str(i)]['item0']) + "\n" + str(arr[str(i)]['item1']) + "\n" + str(arr[str(i)]['item2'])  + "\n" + str(arr[str(i)]['item3'])  + "\n" + str(arr[str(i)]['item4'])  + "\n" + str(arr[str(i)]['item5']), inline=False)
+            embed.add_field(name=i, value = str(arr[str(i)]['item0']) + str(arr[str(i)]['item1'])  + str(arr[str(i)]['item2'])  +  str(arr[str(i)]['item3']) + str(arr[str(i)]['item4']) + str(arr[str(i)]['item5']), inline=False)
             embed.set_author(name=self.client.user.name, icon_url=self.client.user.avatar_url)
             embed.set_footer(text = ctx.author.name, icon_url=ctx.author.avatar_url)
 
